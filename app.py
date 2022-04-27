214709ea5d48f44ec6ccda523cdf56257dea45ff
chensley0216 <100445222+chensley0216@users.noreply.github.com>
2022-04-27 19:32:24 -0400

    Completed 9.3, added app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+import datetime as dt
+from flask import Flask
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
